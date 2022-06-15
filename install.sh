841b21ec6f57b1f6ec9bbe765a5be18a126d8016
Cevdet Arda Haran <76725496+cevdetardaharan@users.noreply.github.com>
2022-06-15 18:20:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,7 +45,50 @@ cd /var/db/kiss/installed && kiss build *
 kiss b e2fsprogs
 kiss b dosfstools
 
+# /var/db/kiss
+curl -FLO https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.47.tar.xz
+tar xvf linux-5.15.47.tar.xz
+cd linux-5.15.47
+
+# /var/db/kiss/linux-5.15.47
+kiss b ncurses
+
+# if you want perl or not || kiss b perl
+patch -p1 < /usr/share/doc/kiss/wiki/kernel/no-perl.patch
+
+# libelf
+kiss b libelf
+sed '/<stdlib.h>/a #include <linux/stddef.h>' tools/objtool/arch/x86/decode.c > _
+mv -f _ tools/objtool/arch/x86/decode.c
+
+# compile kernel
+make && make install
+mv /boot/vmlinuz /boot/vmlinuz-5.15.47
+mv /boot/System.map /boot/System.map-5.15.47
+
+# install init system
+kiss b baseinit
+
 # install efistub
 kiss b efibootmgr
 mount -t efivarfs none /sys/firmware/efi/efivars/
-efibootmgr -c -d /dev/sda -p 1 -L "kiss" -l /vmlinuz-5.15.46-gnu -u "root=PARTUUID={UID} console=tty1 loglevel=0 initcall_debug tsc=reliable rootfstype=ext4"
+efibootmgr -c -d /dev/sda -p 1 -L "kiss" -l /vmlinuz-5.15.47 -u "root=PARTUUID={UID} console=tty1 loglevel=0 initcall_debug tsc=reliable rootfstype=ext4"
+
+# user configuration
+passwd root
+adduser cennedy
+
+# sound
+kiss b alsa-utils
+
+# gpu
+kiss b mesa intel-vaapi-driver
+
+# font
+kiss b hack
+
+# wayland
+kiss b sway-tiny/wlroots foot-pgo grim slurp wl-clipboard wf-recorder wlsunset
+
+# apps
+kiss b htop pfetch mpv firefox rust neovim spotifyd transmission
